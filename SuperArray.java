276ce93f5169d9637b7af107cb107159dda0b56b
Kenli8504 <kli22@stuy.edu>
2020-11-04 12:44:59 -0500

    New class methods; clear, etc.

--- SuperArray.java
+++ SuperArray.java
@@ -42,4 +42,36 @@ public class SuperArray{
     }
     data = data2;
   }
+
+  public boolean isEmpty(){
+    if (size == 0){
+      return true;
+    }
+    return false;
+  }
+
+  public void clear(){
+    String data2 [] = new String[10];
+    data = data2;
+    size = 0;
+  }
+
+  public String toString(){
+    String a = "[";
+    a = a + data[0];
+    for (int i = 1; i < size; i++){
+      a = a + "," + data[i];
+    }
+    a = a + "]";
+    return a;
+  }
+
+  public boolean contains(String s){
+    for (int i = 0; i < size; i ++){
+      if (data[i].equals(s)){
+        return true;
+      }
+    }
+    return false;
+  }
 }
