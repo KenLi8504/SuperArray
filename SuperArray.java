0038724932c9e299f60307a8e833793376da5165
Kenli8504 <kli22@stuy.edu>
2020-11-04 00:36:24 -0500

    Put in resize, editted add method

--- SuperArray.java
+++ SuperArray.java
@@ -12,10 +12,18 @@ public class SuperArray{
   }
 
   public boolean add (String element){
+    if (size != data.length){
       data[size] = element;
       size = size + 1;
       return true;
     }
+    else {
+      resize ();
+      data[size] = element;
+      size = size + 1;
+      return true;
+    }
+  }
 
   public String get(int index){
     return data[index];
@@ -25,5 +33,13 @@ public class SuperArray{
     String a = data[index];
     data[index] = element;
     return a;
+  }
+
+  private void resize (){
+    String data2 [] = new String [size + 5];
+    for (int i = 0; i < size; i ++){
+      data2[i] = data[i];
+    }
+    data = data2;
   }
 }
