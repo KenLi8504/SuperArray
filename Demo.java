d8938fefa7f969daf88141324dd0946e5a051c56
Kenli8504 <kli22@stuy.edu>
2020-11-08 16:46:26 -0500

    Created zip

--- Demo.java
+++ Demo.java
@@ -23,6 +23,34 @@ public class Demo{
     return Intersection;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    SuperArray Alternate = new SuperArray();
+    int sizeA = a.size();
+    int sizeB = b.size();
+    int sizeC = 0;
+    if (sizeA >= sizeB){
+      sizeC = sizeB;
+    }
+    else{
+      sizeC = sizeA;
+    }
+    for (int i = 0; i < sizeC; i++){
+      Alternate.add(a.get(i));
+      Alternate.add(b.get(i));
+    }
+    if (sizeA > sizeB){
+      for (int i = sizeC; i < sizeA; i++){
+        Alternate.add(a.get(i));
+      }
+    }
+    else{
+      for (int i = sizeC; i < sizeB; i++){
+        Alternate.add(b.get(i));
+      }
+    }
+    return Alternate;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     words.add("kani");
@@ -36,6 +64,10 @@ public class Demo{
     words.add("una");
     words.add("ebi");
     words.add("toro");
+    System.out.println(words.lastIndexOf("kani"));
+    System.out.println(words.lastIndexOf("uni"));
+    System.out.println(words.lastIndexOf("ebi"));
+    System.out.println(words.lastIndexOf("una"));
    System.out.println(words);
    removeDuplicates(words);
    System.out.println(words);
@@ -53,5 +85,16 @@ public class Demo{
    setB.add("2");
    setB.add("9");
    System.out.println( findOverlap(setA,setB));
+   SuperArray SetC = new SuperArray();
+   SuperArray SetD = new SuperArray();
+   System.out.println(SetC.equals(SetD));
+   System.out.println(words);
+   System.out.println(setB);
+   words.add("10");
+   words.add("10");
+   words.add("10");
+   words.add("10");
+   setB.add("test");
+   System.out.println(zip(words,setB) );
  }
 }
